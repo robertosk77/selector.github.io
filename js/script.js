47732757d78b2cd4f248fcf6eb085a44c0039da1
Roberto Sanchez <RobertoSK77@gmail.com>
2023-08-12 14:02:29 -0700

    add basic functions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,106 @@
 
+let runningTotal = 0;
+
+function oneChocolate() {
+    runningTotal = runningTotal + 1;
+    console.log(runningTotal);
+
+}
+function oneVanilla() {
+    runningTotal = runningTotal + 2;
+    console.log(runningTotal);
+
+}
+function oneStrawberry() {
+    runningTotal = runningTotal + 3;
+    console.log(runningTotal);
+
+}
+function oneMint() {
+    runningTotal = runningTotal + 4;
+    console.log(runningTotal);
+
+}
+function twoBlue() {
+    runningTotal = runningTotal + 1;
+    console.log(runningTotal);
+
+}
+function twoRed() {
+    runningTotal = runningTotal + 2;
+    console.log(runningTotal);
+
+}
+function twoGreen() {
+    runningTotal = runningTotal + 3;
+    console.log(runningTotal);
+
+}
+function twoYellow() {
+    runningTotal = runningTotal + 4;
+    console.log(runningTotal);
+
+}
+function threeDog() {
+    runningTotal = runningTotal + 1;
+    console.log(runningTotal);
+
+}
+function threeCat() {
+    runningTotal = runningTotal + 2;
+    console.log(runningTotal);
+
+}
+function threeMonkey() {
+    runningTotal = runningTotal + 3;
+    console.log(runningTotal);
+
+}
+function threeElephant() {
+    runningTotal = runningTotal + 4;
+    console.log(runningTotal);
+
+}
+function fourChina() {
+    runningTotal = runningTotal + 1;
+    console.log(runningTotal);
+
+}
+function fourFrance() {
+    runningTotal = runningTotal + 2;
+    console.log(runningTotal);
+
+}
+function fourPeru() {
+    runningTotal = runningTotal + 3;
+    console.log(runningTotal);
+
+}
+function fourEthiopia() {
+    runningTotal = runningTotal + 4;
+    console.log(runningTotal);
+
+}
+function fiveJohn() {
+    runningTotal = runningTotal + 1;
+    console.log(runningTotal);
+
+}
+function fivePaul() {
+    runningTotal = runningTotal + 2;
+    console.log(runningTotal);
+
+}
+function fiveGeorge() {
+    runningTotal = runningTotal + 3;
+    console.log(runningTotal);
+
+}
+function fiveRingo() {
+    runningTotal = runningTotal + 4;
+    console.log(runningTotal);
+
+}
+
 
 
