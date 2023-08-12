69ceb83cee1818a4c98c5a90333bdbb4192355b4
Roberto Sanchez <RobertoSK77@gmail.com>
2023-08-11 20:36:12 -0700

    add DOM

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const ul_1 = document.querySelector("answers1");
+const ul_2 = document.querySelector("answers2");
+const ul_3 = document.querySelector("answers3");
+const ul_4 = document.querySelector("answers4");
+const ul_5 = document.querySelector("answers5");
+
+const q1 = document.querySelector(".question1");
+const q2 = document.querySelector(".question2");
+const q3 = document.querySelector(".question3");
+const q4 = document.querySelector(".question4");
+const q5 = document.querySelector(".question5");
+
+const instructions = document.querySelector(".instructions");
+const results = document.querySelector(".results");
