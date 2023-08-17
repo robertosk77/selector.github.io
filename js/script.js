c783a46f1cf76892f7c0cfb2452969351b277ffc
Roberto Sanchez <RobertoSK77@gmail.com>
2023-08-16 20:05:46 -0700

    update functions

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ function runRingoRun(){
     window.alert("Seriously? Wow. Ok.");
 }
 function runHTML(){
+    document.getElementById("results").value = "HTML & CSS";
+    document.write(document.getElementById("results").innerHTML);
     
 }
 function runPython(){
