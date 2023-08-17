591a7bddff39db9085730101d4f57bca92570129
Roberto Sanchez <RobertoSK77@gmail.com>
2023-08-16 19:52:41 -0700

    add functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
+function runRingoRun(){
+    window.alert("Seriously? Wow. Ok.");
+}
+function runHTML(){
+    
+}
+function runPython(){
 
+}
+function runJavaScript(){
 
+}
+function runEnglish(){
 
-
-
+}
 
