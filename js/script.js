5a0c23f86cbaf1923c3b7cc11e1d525b3036267a
Roberto Sanchez <RobertoSK77@gmail.com>
2023-08-19 16:51:42 -0700

    remove functions

--- js/script.js
+++ js/script.js
@@ -1,30 +1,4 @@
 window.onload;
 document.querySelector('button')
 
-function runHTML(){
-    
-    document.getElementById("results").value = "HTML & CSS";
-    document.write(document.getElementById("results").innerHTML);
-    document.write("<h1>HTML & CSS!!!</h1>");
-    document.write("<p>This is the best pair of programming languages to start out with, as all web development relies on them. The CODE WIZARD commands you to learn it at once!!!</p>");
-    
-}
-function runPython(){
-    document.getElementById("results").value = "PYTHON";
-    document.write(document.getElementById("results").innerHTML);
-    document.write("<h1>PYTHON!!!</h1>");
-    document.write("<p>This is a programming language that confuses and frightens the CODE WIZARD. You are truly a brave soul if you learn this language. And you will! Because the CODE WIZARD commands it!!!</p>");
-}
-function runJavaScript(){
-    document.getElementById("results").value = "JAVASCRIPT";
-    document.write(document.getElementById("results").innerHTML);
-    document.write("<h1>JAVASCRIPT!!!</h1>");
-    document.write("<p>This programming language adds interactive elements to your websites, and this website would not be possible without it! You will have a lot of fun with this language. The CODE WIZARD commands it!!!</p>");
-}
-function runEnglish(){
-    document.getElementById("results").value = "HTML & CSS";
-    document.write(document.getElementById("results").innerHTML);
-    document.write("<h1>ENGLISH!!!</h1>");
-    document.write("<p>What's the matter, can't you read? No refunds! The CODE WIZARD commands it! Now get outta here! Or take the quiz again... IF YOU DARE!!!</p>");
-}
 
